d3d1ab874d3f4b451b972c1c9ea09481217c02a1
José Nery <josenerydev@gmail.com>
2022-10-10 20:09:10 -0300

    oledb example

---
+++ OleDBExamples/Extensions/ExcelExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Data;
+using System.Data.OleDb;
+
+namespace OleDBExamples.Extensions
+{
+    public class ExcelExtensions
+    {
+        public static List<T> XlsxToObject<T>(string path, string commandText)
+        {
+            using (OleDbConnection connection = new OleDbConnection())
+            {
+                DataTable dataTable = new DataTable();
+                connection.ConnectionString = $"Provider=Microsoft.ACE.OLEDB.12.0; Data Source={path};Extended Properties=Excel 12.0;";
+                using (OleDbCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = commandText;
+                    using (OleDbDataAdapter adapter = new OleDbDataAdapter())
+                    {
+                        adapter.SelectCommand = command;
+                        adapter.Fill(dataTable);
+                    }
+                }
+
+                return JsonConvert.DeserializeObject<List<T>>(JsonConvert.SerializeObject(dataTable));
+            }
+        }
+    }
+}
--- OleDBExamples/Program.cs
+++ OleDBExamples/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace OleDBExamples
+﻿using OleDBExamples.Extensions;
+using OleDBExamples.Models;
+
+namespace OleDBExamples
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Product> products = new List<Product>();
+
+            const string commandText = "SELECT CInt(Id) AS Id, CStr(Name) AS Name FROM [Sheet1$]";
+            var fileName = "C:\\Temp\\products.xlsx";
+            try
+            {
+                products = ExcelExtensions.XlsxToObject<Product>(fileName, commandText);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
